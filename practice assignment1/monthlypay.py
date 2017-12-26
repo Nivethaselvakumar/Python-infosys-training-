fb7d38b063797a97a435225e20dc196f8037e860
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2017-12-26 15:48:37 +0530

    Create monthlypay.py

---
+++ practice assignment1/monthlypay.py
@@ -0,0 +1,8 @@
+number_of_hours=40
+pay_rate_perhour=400
+no_of_weeks=4
+monthly_pay=number_of_hours*pay_rate_perhour*no_of_weeks
+print("number of hours worked in a week:",number_of_hours)
+print("pay rate per hour is:",pay_rate_perhour)
+print("number of weeks in a month:",no_of_weeks)
+print("monthly pay:",monthly_pay)
