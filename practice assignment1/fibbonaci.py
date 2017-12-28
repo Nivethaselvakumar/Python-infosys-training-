e4000633b6f3cdc22766d2d19c70890cdc280045
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2017-12-28 12:21:34 +0530

    Update fibbonaci.py

--- practice assignment1/fibbonaci.py
+++ practice assignment1/fibbonaci.py
@@ -1,6 +1,7 @@
 nterms=int(input("enter a number"))
 n1 = 0
 n2 = 1
+l=[]
 count = 0
 if nterms <= 0:
    print("Please enter a positive integer")
@@ -10,11 +11,13 @@ elif nterms == 1:
 else:
    print("Fibonacci sequence upto",nterms,":")
    while count < nterms:
-       print(n1,end=',')
+       l.append(n1)
        nth = n1 + n2
        
        n1 = n2
        n2 = nth
        count += 1
+       
+   print(l)
       
        
